03a8e5d59f3d6a187377f00e8fba7a23d8be540f
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-09 22:39:21 +0300

    Updating Category and favorte

--- database/migrations/2022_06_09_193550_create_favortes_table.php
+++ database/migrations/2022_06_09_193550_create_favortes_table.php
@@ -15,6 +15,8 @@ class CreateFavortesTable extends Migration
     {
         Schema::create('favortes', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('item_id');
             $table->timestamps();
         });
     }

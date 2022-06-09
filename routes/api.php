48cae79addb4420f11f69b8305da149a176ecbce
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-09 19:50:07 +0300

    Updating api.php adding group:v1

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::group(['middleware' => 'api'], function($router) {
-    Route::post('/register', [UserController::class, 'register']);
-    Route::post('/login', [UserController::class, 'login']);
-    Route::post('/logout', [UserController::class, 'logout']);
-    Route::post('/refresh', [UserController::class, 'refresh']);
-    Route::post('/profile', [UserController::class, 'profile']);
-}); 
+
+Route::group(['prefix' => 'v1'], function(){
+    Route::group(['middleware' => 'api'], function($router) {
+        Route::post('/register', [UserController::class, 'register']);
+        Route::post('/login', [UserController::class, 'login']);
+        Route::post('/logout', [UserController::class, 'logout']);
+        Route::post('/refresh', [UserController::class, 'refresh']);
+        Route::post('/profile', [UserController::class, 'profile']);
+    });
+});
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();

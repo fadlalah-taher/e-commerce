cdf76373b7277f935fddb085d18bbdfb56883343
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-10 17:56:17 +0300

    Adding DOM in login.js

--- e-commerce-frontend/javascript/login.js
+++ e-commerce-frontend/javascript/login.js
@@ -1,3 +1,24 @@
 // buttons
 var signBtn = document.getElementById("signBtn");
 var loginBtn = document.getElementById("loginBtn");
+
+
+// hide password icons
+var hide1 = document.getElementById("hide1");
+var hide2 = document.getElementById("hide2");
+var passwordLoginIcon = document.getElementById("password-icon");
+var passwordRegisterIcon = document.getElementById("passwordRegister-icon");
+var loginInputPassword = document.getElementById("login-inputPassword");
+var registerInputPassword = document.getElementById("inputPassword-register");
+var popup = document.querySelector(".popup");
+
+
+// email and register popup message
+var emailInput = document.getElementById("emailInput");
+var registerSection = document.getElementById("register-section");
+var invalidEmail = document.getElementById("invalidEmail");
+var createdAccount = document.getElementById("createdAccount");
+
+/* register */
+var createBtn = document.getElementById("createBtn");
+var registerForm = document.getElementById("createAcount");

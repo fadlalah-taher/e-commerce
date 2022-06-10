6f85300e4192513f92b0420c7fdffc2d8411af6c
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-10 18:05:10 +0300

    Adding on click function that unhide password

--- e-commerce-frontend/javascript/login.js
+++ e-commerce-frontend/javascript/login.js
@@ -6,6 +6,8 @@ var loginBtn = document.getElementById("loginBtn");
 // hide password icons
 var hide1 = document.getElementById("hide1");
 var hide2 = document.getElementById("hide2");
+var hide1 = document.getElementById("hide3");
+var hide2 = document.getElementById("hide4");
 var passwordLoginIcon = document.getElementById("password-icon");
 var passwordRegisterIcon = document.getElementById("passwordRegister-icon");
 var loginInputPassword = document.getElementById("login-inputPassword");
@@ -35,6 +37,22 @@ passwordLoginIcon.addEventListener("click", function(){
         loginInputPassword.type = "password";
         hide1.style.display = "none";
         hide2.style.display = "block";
+  
+      }
+  });
+
+
+  // hide password Register form
+  passwordRegisterIcon.addEventListener("click", function(){
+    if(registerInputPassword.type === 'password'){
+        registerInputPassword.type = "text";
+        hide3.style.display = "block";
+        hide4.style.display = "none";
+      }
+      else{
+        registerInputPassword.type = "password";
+        hide3.style.display = "none";
+        hide4.style.display = "block";
   
       }
   });

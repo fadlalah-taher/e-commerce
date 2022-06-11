c679facc6ccb2a84dfd6d8d34852eee56bb505da
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-11 12:20:24 +0300

    Cleaning preload.js and index.js

--- electron/admin/src/index.js
+++ electron/admin/src/index.js
@@ -50,28 +50,4 @@ app.on('activate', () => {
 // code. You can also put them in separate files and import them here.
 
 
-// DOM
-
-// var passwordLoginIcon = document.getElementById("password-icon");
-// var loginInputPassword = document.getElementById("login-inputPassword");
-
-// // hide/unhide password icons DOM
-// var hide1 = document.getElementById("hide1");
-// var hide2 = document.getElementById("hide2");
-
-// // hide/unhide password login form
-// passwordLoginIcon.addEventListener("click", function(){
-//   if(loginInputPassword.type === 'password'){
-//       loginInputPassword.type = "text";
-//       hide1.style.display = "block";
-//       hide2.style.display = "none";
-//     }
-//     else{
-//       loginInputPassword.type = "password";
-//       hide1.style.display = "none";
-//       hide2.style.display = "block";
-
-//     }
-// });
-
 

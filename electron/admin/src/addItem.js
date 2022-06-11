2be06ff5bf98527548f819f7edaf4821c219f3bf
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-11 15:52:28 +0300

    Adding addItem.js and onclick add item

---
+++ electron/admin/src/addItem.js
@@ -0,0 +1,35 @@
+const axios = require('axios').default;
+
+window.addEventListener('DOMContentLoaded', () => {
+
+
+    // DOM AddItem
+    var addItemForm = document.getElementById("createItem");
+    var addBtn = document.getElementById("addBtn");
+  // Add Item
+
+  addBtn.addEventListener("click", function(){
+
+      let data = new FormData(addItemForm);
+      axios({
+          method: 'post',
+          url: 'http://127.0.0.1:8000/api/additem',
+          data: data,
+      })
+      .then(function (response) {
+        if(response.data == "mess around"){
+          createdAccount.style.display = "block";
+        }
+        else{
+          console.log(response);
+          console.log(response.data);
+          //window.location = "file:///C:/Users/Fadel/e-commerce/e-commerce-backend/e-commerce-frontend/login.html#";
+        }
+      }
+      )
+    });
+
+    for (const type of ['chrome', 'node', 'electron']) {
+        //replaceText(`${type}-version`, process.versions[type])
+    }
+});
--- electron/admin/src/preload.js
+++ electron/admin/src/preload.js
@@ -18,16 +18,43 @@ window.addEventListener('DOMContentLoaded', () => {
         axios({
             method: 'post',
             url: 'http://127.0.0.1:8000/api/login',
+            headers: {Authorization: `Bearer ${data.token}`},
             data: data,
         })
         .then(function (response) {
-            console.log(response.data);
+            //console.log(response['data']['access_token']);
             console.log(response.data.access_token);
             console.log("successfull");
-            //window.location = "file:///C:/Users/Fadel/e-commerce/e-commerce-backend/e-commerce-frontend/index.html";
+            window.location = "addItem.html";
           }
         ) 
       });
+
+    //   // DOM AddItem
+    //   var addItemForm = document.getElementById("createItem");
+    //   var addBtn = document.getElementById("addBtn");
+    // // Add Item
+
+    // addBtn.addEventListener("click", function(){
+
+    //     let data = new FormData(addItemForm);
+    //     axios({
+    //         method: 'post',
+    //         url: 'http://127.0.0.1:8000/api/additem',
+    //         data: data,
+    //     })
+    //     .then(function (response) {
+    //       if(response.data == "mess around"){
+    //         createdAccount.style.display = "block";
+    //       }
+    //       else{
+    //         console.log(response);
+    //         console.log(response.data);
+    //         //window.location = "file:///C:/Users/Fadel/e-commerce/e-commerce-backend/e-commerce-frontend/login.html#";
+    //       }
+    //     }
+    //     )
+    //   });
 
 
 

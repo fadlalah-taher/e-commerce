aff8706ee2c827f9b6f083ddcea05ebb87768b0e
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-11 10:23:51 +0300

    Adding form inside addItem.html

--- electron/admin/src/index.js
+++ electron/admin/src/index.js
@@ -46,4 +46,29 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
+
 // DOM
+
+// var passwordLoginIcon = document.getElementById("password-icon");
+// var loginInputPassword = document.getElementById("login-inputPassword");
+
+// // hide/unhide password icons DOM
+// var hide1 = document.getElementById("hide1");
+// var hide2 = document.getElementById("hide2");
+
+// // hide/unhide password login form
+// passwordLoginIcon.addEventListener("click", function(){
+//   if(loginInputPassword.type === 'password'){
+//       loginInputPassword.type = "text";
+//       hide1.style.display = "block";
+//       hide2.style.display = "none";
+//     }
+//     else{
+//       loginInputPassword.type = "password";
+//       hide1.style.display = "none";
+//       hide2.style.display = "block";
+
+//     }
+// });
+
+

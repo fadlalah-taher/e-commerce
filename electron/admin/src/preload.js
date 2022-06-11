2f572f8da6b994e4ef3874c03c3073a44aff81bb
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-11 12:18:30 +0300

    Adding and updating on preload.js

--- electron/admin/src/preload.js
+++ electron/admin/src/preload.js
@@ -1,22 +1,51 @@
 
-var passwordLoginIcon = document.getElementById("password-icon");
-var loginInputPassword = document.getElementById("login-inputPassword");
+window.addEventListener('DOMContentLoaded', () => {
+    // DOM
+    var passwordLoginIcon = document.getElementById("password-icon");
+    var loginInputPassword = document.getElementById("login-inputPassword");
 
-// hide/unhide password icons DOM
-var hide1 = document.getElementById("hide1");
-var hide2 = document.getElementById("hide2");
+    // hide/unhide password icons DOM
+    var hide1 = document.getElementById("hide1");
+    var hide2 = document.getElementById("hide2");
 
-// hide/unhide password login form
-passwordLoginIcon.addEventListener("click", function(){
-  if(loginInputPassword.type === 'password'){
-      loginInputPassword.type = "text";
-      hide1.style.display = "block";
-      hide2.style.display = "none";
+    // hide/unhide password login form
+    passwordLoginIcon.addEventListener("click", function(){
+    if(loginInputPassword.type === 'password'){
+        loginInputPassword.type = "text";
+        hide1.style.display = "block";
+        hide2.style.display = "none";
+        }
+        else{
+        loginInputPassword.type = "password";
+        hide1.style.display = "none";
+        hide2.style.display = "block";
+
+        }
+    });
+
+  
+    for (const type of ['chrome', 'node', 'electron']) {
+      //replaceText(`${type}-version`, process.versions[type])
     }
-    else{
-      loginInputPassword.type = "password";
-      hide1.style.display = "none";
-      hide2.style.display = "block";
+  })
+// var passwordLoginIcon = document.getElementById("password-icon");
+// var loginInputPassword = document.getElementById("login-inputPassword");
 
-    }
-});
+// // hide/unhide password icons DOM
+// var hide1 = document.getElementById("hide1");
+// var hide2 = document.getElementById("hide2");
+
+// // hide/unhide password login form
+// passwordLoginIcon.addEventListener("click", function(){
+//   if(loginInputPassword.type === 'password'){
+//       loginInputPassword.type = "text";
+//       hide1.style.display = "block";
+//       hide2.style.display = "none";
+//     }
+//     else{
+//       loginInputPassword.type = "password";
+//       hide1.style.display = "none";
+//       hide2.style.display = "block";
+
+//     }
+// });

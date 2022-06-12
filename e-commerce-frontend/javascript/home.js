6016128afc2b3cc56f4f233bb4388ef1a30122fd
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-12 10:20:38 +0300

    Adding on home.js function onload>axios

--- e-commerce-frontend/javascript/home.js
+++ e-commerce-frontend/javascript/home.js
@@ -1,5 +1,6 @@
 var myNav = document.getElementById("myNav");
 var icon = document.getElementById("span-icon");
+var itemContainer = document.getElementById("item-container");
 
 icon.addEventListener("click", function(){
     myNav.style.width = "100%";
@@ -7,3 +8,39 @@ icon.addEventListener("click", function(){
 myNav.addEventListener("click", function(){
     myNav.style.width = "0%";
 });
+
+
+window.onload = (function(){
+    axios({
+        method: 'get',
+        url: 'http://127.0.0.1:8000/api/v1/item/allitems',
+    })
+    .then(function (response) {
+        console.log("hi");
+        console.log(response);
+        let items = response.data['items'];
+        console.log(items);
+        itemContainer.innerHTML = ""; 
+        items.forEach(item => {
+            let userHtml = `
+            <div class="column">
+            <div class="card">
+              <img src="assets/whiteshirt.jpeg" alt="" />
+              <div class="card-caption">
+                <h4><b>${item.name}</b></h4>
+                <p>
+                ${item.description}
+                </p>
+                <p>
+                ${item.price}
+                </p>
+                <i class="fa-solid fa-heart"></i>
+              </div>
+            </div>
+          </div>`;
+          itemContainer.innerHTML +=userHtml;
+        })
+        console.log(response);
+        }
+    )
+})

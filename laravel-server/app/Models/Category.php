47a6f8c8fe871e5232e59966e17d11f7a912acc8
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-12 19:10:04 +0300

    Adding relation between category and item

--- laravel-server/app/Models/Category.php
+++ laravel-server/app/Models/Category.php
@@ -9,8 +9,8 @@ class Category extends Model
 {
     use HasFactory;
 
-    // public function items()
-    // {
-    //     return $this->hasMany(Item::class);
-    // }
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
 }
